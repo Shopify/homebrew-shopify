641fdfce5eee2093ac820a5925734401072207d7
Simon Eskildsen <sirup@sirupsen.com>
2015-01-06 14:12:20 -0500

    initial: toxiproxy

---
+++ toxiproxy.rb
@@ -0,0 +1,16 @@
+# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
+#                /usr/local/Library/Contributions/example-formula.rb
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
+class Toxiproxy < Formula
+  homepage "https://github.com/Shopify/toxiproxy"
+  url "http://shopify-vagrant.s3.amazonaws.com/toxiproxy/1.0.0-darwin-amd64"
+  sha1 "05de584c56572330e6dd5c1cb2d38a5f388fe76e"
+  version "1.0.0rc1"
+
+  depends_on :arch => :x86_64
+
+  def install
+    bin.install "1.0.0-darwin-amd64" => "toxiproxy"
+  end
+end
