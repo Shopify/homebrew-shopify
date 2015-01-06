fd4f95c2fea34d4a97010be02c889e446be51660
Simon Eskildsen <sirup@sirupsen.com>
2015-01-06 14:33:06 -0500

    toxiproxy: add plist

--- toxiproxy.rb
+++ toxiproxy.rb
@@ -13,4 +13,26 @@ class Toxiproxy < Formula
   def install
     bin.install "1.0.0-darwin-amd64" => "toxiproxy"
   end
+
+  plist_options :manual => "toxiproxy"
+
+  def plist; <<-EOS.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
+    <plist version="1.0">
+      <dict>
+        <key>Label</key>
+        <string>#{plist_name}</string>
+        <key>ProgramArguments</key>
+        <array>
+          <string>#{opt_bin}/toxiproxy</string>
+        </array>
+        <key>RunAtLoad</key>
+        <true/>
+        <key>KeepAlive</key>
+        <true/>
+      </dict>
+    </plist>
+    EOS
+  end
 end
