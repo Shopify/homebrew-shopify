5759603c85f62c4e796ea12050e10f7b5315eeec
Simon Eskildsen <sirup@sirupsen.com>
2015-01-07 11:35:21 -0500

    toxiproxy: update to 1.0.0

--- toxiproxy.rb
+++ toxiproxy.rb
@@ -4,9 +4,9 @@
 
 class Toxiproxy < Formula
   homepage "https://github.com/Shopify/toxiproxy"
-  url "http://shopify-vagrant.s3.amazonaws.com/toxiproxy/1.0.0-darwin-amd64"
+  url "https://github.com/Shopify/toxiproxy/releases/download/v1.0.0/toxiproxy-darwin-amd64"
   sha1 "05de584c56572330e6dd5c1cb2d38a5f388fe76e"
-  version "1.0.0rc1"
+  version "1.0.0"
 
   depends_on :arch => :x86_64
 
