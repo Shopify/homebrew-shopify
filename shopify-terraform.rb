1eabf798e8dfb32de901030b5f551c9548f00f82
Jason Gedge <thegedge@users.noreply.github.com>
2015-11-19 19:52:11 -0500

    Bump shopify-terraform version to v0.6.7-pre

--- shopify-terraform.rb
+++ shopify-terraform.rb
@@ -1,14 +1,14 @@
 class ShopifyTerraform < Formula
   desc "Shopify fork of Terraform"
   homepage "https://github.com/Shopify/shopify-terraform"
-  url "https://s3.amazonaws.com/shopify-terraform-binaries/darwin_amd64_14f4b911024580611dbe59707d50083b61cd7283.tar.gz"
-  sha256 "3ac3bdd6bfc652690b3001f7c2d12dd09d51293cd3d6c9656018936b4494f418"
-  version "0.6.4-shopify-1"
+  url "https://s3.amazonaws.com/shopify-terraform-binaries/darwin_amd64_c2617c96cd3c8369981715b115173de7157f2809.tar.gz"
+  sha256 "82418314cdd2fd92371121e6fb029f363dfc538aab09493a6b7c4e64e55f2e0f"
+  version "0.6.7-pre-shopify"
 
   conflicts_with "terraform", :because => "both install terraform binaries"
 
   def install
-    Dir.glob('terraform*').each { |f| bin.install f}
+    Dir.glob('terraform*').each { |f| bin.install f }
   end
 
   test do
