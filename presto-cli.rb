5f1e4320a21a3d52d3505f85ff3391180a6fea7f
John Duff <duff.john@gmail.com>
2016-08-05 11:15:05 -0400

    Missed updating one of the versions in the URL.

--- presto-cli.rb
+++ presto-cli.rb
@@ -1,7 +1,7 @@
 class PrestoCli < Formula
   desc "Presto CLI executable to connect and run queries against Presto"
   homepage "https://prestodb.io/docs/current/installation/cli.html"
-  url "https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.150/presto-cli-0.151-executable.jar"
+  url "https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.151/presto-cli-0.151-executable.jar"
   sha256 "cf7032e820e88cce76e2cc92710a5bf82c459588c2d628165d084438e18a35ff"
   version "0.151"
 
