bfd68b5a6e54bd766ca75d6c5a365583fc3d28d4
John Duff <duff.john@gmail.com>
2016-08-05 11:20:33 -0400

    Update the sha for the new jar

--- presto-cli.rb
+++ presto-cli.rb
@@ -2,7 +2,7 @@ class PrestoCli < Formula
   desc "Presto CLI executable to connect and run queries against Presto"
   homepage "https://prestodb.io/docs/current/installation/cli.html"
   url "https://repo1.maven.org/maven2/com/facebook/presto/presto-cli/0.151/presto-cli-0.151-executable.jar"
-  sha256 "cf7032e820e88cce76e2cc92710a5bf82c459588c2d628165d084438e18a35ff"
+  sha256 "7d8cde7a38080a08425445e200dc7ecb26635204c32769a59277133bf6328fbb"
   version "0.151"
 
   def install
