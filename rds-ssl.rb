86ab9c2a6901d477e751768391f2234b9acbfb6b
Sam Talasila <sam.talasila@shopify.com>
2016-10-06 13:10:27 -0400

    updated the expected sha
    to account for
    ```Error: SHA256 mismatch
    Expected: 90567481d386dac26f4c734d83bbe8576955b2a6c84b00fc5113cdf1b7d87ea0
    Actual: d8ef0551c3d97d91cc58dc73cd24f2f1d09d992276009d747252fc1dc2c5edef```

--- rds-ssl.rb
+++ rds-ssl.rb
@@ -2,7 +2,7 @@ class RdsSsl < Formula
   desc "Root public key for Amazon redshift SSL connectivity"
   homepage "http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Aurora.Connect.html"
   url "http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem"
-  sha256 "90567481d386dac26f4c734d83bbe8576955b2a6c84b00fc5113cdf1b7d87ea0"
+  sha256 "d8ef0551c3d97d91cc58dc73cd24f2f1d09d992276009d747252fc1dc2c5edef"
   version "0.2.0"
 
   def install
