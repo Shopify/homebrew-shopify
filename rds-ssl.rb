5cabb5cc604007cad5b3652073f8ee1c7f986a04
Jason White <jason.white@shopify.com>
2016-10-26 10:41:12 -0400

    updates SHA for rds-ssl package

--- rds-ssl.rb
+++ rds-ssl.rb
@@ -2,7 +2,7 @@ class RdsSsl < Formula
   desc "Root public key for Amazon redshift SSL connectivity"
   homepage "http://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Aurora.Connect.html"
   url "http://s3.amazonaws.com/rds-downloads/rds-combined-ca-bundle.pem"
-  sha256 "d8ef0551c3d97d91cc58dc73cd24f2f1d09d992276009d747252fc1dc2c5edef"
+  sha256 "58c92aea3a96591abdd0c6ebb4116d9e33af946196bc1e43196961b1b0eac415"
   version "0.2.0"
 
   def install
