2d75830b02c4eae7b3edd25a81160fef9e324f60
Burke Libbey <burke@libbey.me>
2016-11-24 16:16:01 -0500

    re-bottle

--- yarn.rb
+++ yarn.rb
@@ -8,10 +8,10 @@ class Yarn < Formula
   head "https://github.com/yarnpkg/yarn.git"
 
   bottle do
+    root_url "https://s3.amazonaws.com/burkelibbey"
     cellar :any_skip_relocation
-    sha256 "01ab99abd61a20a10eb99ebcfe2d5ce2fc49671848531ab1a0f21728e9cf8a3f" => :sierra
-    sha256 "4c2f2ed16e4c529c7a77e09d8107ce4392e4e8cedff10bee76d4193c0d8ad420" => :el_capitan
-    sha256 "4842ee650f3af9e6794bfa877fd2459a55cb60909b62193c97df45e5097c8b63" => :yosemite
+    rebuild 1
+    sha256 "5fd2381f31aa782547229832659d816cc112f6cb1050f007b4828b27f6b45485" => :sierra
   end
 
   depends_on "node"
