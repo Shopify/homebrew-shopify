5f29ab887c4e071cfcb825513174a5c63e5ff2ed
Tim Anema <tim.anema@shopify.com>
2017-01-20 13:21:58 -0500

    updating to critical version that previous version will be broken in the future

--- themekit.rb
+++ themekit.rb
@@ -1,9 +1,9 @@
 class Themekit < Formula
   desc 'Theme Kit is a tool kit for manipulating shopify themes'
   homepage 'https://shopify.github.io/themekit/'
-  url 'https://shopify-themekit.s3.amazonaws.com/v0.6.1/darwin-amd64/theme'
-  sha256 'a3ddaec8b8bc7b0dee76e905041fd28d3b9c7ea4fd0a43299cd3155f3dd51197'
-  version 'v0.6.1'
+  url 'https://shopify-themekit.s3.amazonaws.com/v0.6.4/darwin-amd64/theme'
+  sha256 '00781aaf68767e1cfebec819dd282c710950d31a47fcfb651ef557568cbe6a48'
+  version 'v0.6.4'
 
   def install
     bin.install 'theme'
