54064a2ae4e5c9cc025c2e67d4c672815280be81
Peng Yu <peng.yu@shopify.com>
2017-09-08 11:19:01 -0400

    change the sha

--- redshift-ssl.rb
+++ redshift-ssl.rb
@@ -2,7 +2,7 @@ class RedshiftSsl < Formula
   desc "Root public key for Amazon redshift SSL connectivity"
   homepage "http://docs.aws.amazon.com/redshift/latest/mgmt/connecting-ssl-support.html#connect-using-ssl"
   url "https://s3.amazonaws.com/redshift-downloads/redshift-ssl-ca-cert.pem"
-  sha256 "66fdaaa6ea623fce7052c5b77948d1482317f27076040100167840d3fa2bb449"
+  sha256 "f63b3098250346eee57b63a2b150cb4fd75c3bb70bdd36bc1ac7de6d462499e4"
   version "0.1.0"
 
   def install
