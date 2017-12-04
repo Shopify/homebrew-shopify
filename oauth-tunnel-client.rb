feadc31dd6f10af4d7f1d62fad3b3796d4d17f42
John Martin <johnjosephmartin@icloud.com>
2017-12-04 16:13:26 -0500

    Upping signed url time to 30d

--- oauth-tunnel-client.rb
+++ oauth-tunnel-client.rb
@@ -7,7 +7,7 @@ class OauthTunnelClient < Formula
 
   desc 'Create a secure local proxy with Shopify GCP services'
   homepage 'https://github.com/Shopify/oauth-tunnel-client'
-  url 'https://storage.googleapis.com/oauth-tunnel-binaries/oauth-tunnel-client-binaries-08e20d74b6992411aa203d296eb7ed7a1ac089ba.tar.gz?GoogleAccessId=pipa-production@shopify-docker-images.iam.gserviceaccount.com&Expires=1513005948&Signature=mpd7rbZa%2BxC0ceVrluc8H9uxwrfOuJdGYvn0eLzsI%2BuN41Pyfe3tkMAdepTrpOHu8MUm4ADovk8FJHxdtWbWdqkkTHZkw8NGF4VOfKIzqGXVPzE58DGMaxw2B2AFSj4rFCApltHxesW6Hd%2Fo%2BIf%2BryWQ9AjpKLXchy%2B%2BOB0FZxMuayLbA5aazLr%2B%2BGrA7XW7tFuwjzlgt%2F8aqy7qvtOdAcG5V8dvKw0DV9UcZb5GlveNQdqSaYEBst85eNRDTq%2B8uKvs1cd%2FVQTfyrymHa3bC%2BRFyvHEuQKk33dhknUxjnb%2Fue%2FSkuEDBmUd8JL3QzZ8SGuC2GEXxLgGQ2X%2BzOcqXw%3D%3D', using: GoogleStorageDownloadStrategy
+  url 'https://storage.googleapis.com/oauth-tunnel-binaries/oauth-tunnel-client-binaries-08e20d74b6992411aa203d296eb7ed7a1ac089ba.tar.gz?GoogleAccessId=data-gcs-readonly@data-148903.iam.gserviceaccount.com&Expires=1515013862&Signature=w%2Fk%2BSrQgwvuSV98GLCl%2B31j3iaocbpMQ%2FYZV1%2FLcYsiQRQR2a9j9biZdjY1c777KhaOGo0MnCsAsSJMbYQdYWA3KKHFfj4Bv%2FnuWzL0%2FucCNF%2Fd6US1EMIPqjSBixfIxrHXFY9p6X%2F1mMD3vr6Ua5ZChe2p%2BG516b1MCQu4cK67tLRTiW%2FEycyYZ7tcmwU4%2BFNwqBEAyc4rZhwJMwKCDfPxdFaRXLMnRN6J95qpZYQa2dBsHG7v6gESpkQIvHyga%2FXep%2FSwt4KGOpZq2K3Bp8tz1Q%2FeiK2KAsGc6oYnjShDDlXFHWNeAg%2FI76Dhee6c0tNlbCAbr5oKXqjtUy6OIKg%3D%3D', using: GoogleStorageDownloadStrategy
   sha256 "8a8cfc2305093f5c255899380327d0441f72191231bb38f7dfb6c8101baf1195"
   version "0.1"
 
