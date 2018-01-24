43947d1fea98a7e81c5fb724e33f3bdb994e6799
Francisco Mejia <francisco.mejia@shopify.com>
2018-01-24 14:25:03 -0500

    Fix 'make install' permission/privilege error

--- libcidr-shopify.rb
+++ libcidr-shopify.rb
@@ -1,12 +1,11 @@
 class LibcidrShopify < Formula
   desc 'Libcidr is a library that provides utility functions to handle IP addresses and net blocks.'
   homepage 'https://github.com/Shopify/libcidr-shopify/'
-  url 'https://github.com/Shopify/libcidr-shopify/archive/v1.1.0.tar.gz'
-  sha256 'ba1fc083dbf39a902bf17add9628b3e5b988385d7a1e8006293372e1094f11e6'
+  url 'https://github.com/Shopify/libcidr-shopify/archive/v1.0.0.tar.gz'
+  sha256 '718fbb00ab1ae8f9e692ce6f917ee7b485420df1e05537f8ab871de6a667915d'
 
   def install
     system "make"
-    system "make", "install"
     lib.install "./src/libcidr.dylib"
     lib.install "./src/libcidr.dylib.0"
   end
