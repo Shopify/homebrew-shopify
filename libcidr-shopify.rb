778c0657b27575955bd9c3a7ccb5b3c658c98250
Francisco Mejia <francisco.mejia@shopify.com>
2018-01-26 14:03:32 -0500

    Refactor Makefile compatibility tweaks

--- libcidr-shopify.rb
+++ libcidr-shopify.rb
@@ -1,12 +1,12 @@
 class LibcidrShopify < Formula
   desc 'Libcidr is a library that provides utility functions to handle IP addresses and net blocks.'
-  homepage 'https://github.com/Shopify/libcidr-shopify/'
+  homepage 'https://www.over-yonder.net/~fullermd/projects/libcidr'
   url 'http://bazaar.launchpad.net/~fullermd/libcidr/trunk/tarball/302'
   sha256 'cb3c51ffec7c6dd83dca1e7f3a92852e4febd230624e84457c269bec82432719'
 
   patch do
-    url "https://gist.githubusercontent.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8/raw/fb499d61e4645c04195a84deb293b2f7a2a4720e/mac_os_libcidr_support.patch"
-    sha256 "b0400ea3bff166c189365c283874e0ff8888a6db89f7a0c541eff88dcd1f2b84"
+    url "https://gist.githubusercontent.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8/raw/7888c4a21a1e2afcaf0cdff627d5643574d020a0/mac_os_libcidr_support.patch"
+    sha256 "8578885fd5d8a5c5bf64b92283292c568ba8b38bf7d411a79b0533cfee8a546f"
   end
 
   def install
