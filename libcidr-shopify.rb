3b468e634f3084d1ad2eed26d8b24fd2c61bfbfe
Francisco Mejia <francisco.mejia@shopify.com>
2018-01-27 17:04:46 -0500

    Modify libcidr upstream host and update patch

--- libcidr-shopify.rb
+++ libcidr-shopify.rb
@@ -1,18 +1,15 @@
 class LibcidrShopify < Formula
   desc 'Libcidr is a library that provides utility functions to handle IP addresses and net blocks.'
   homepage 'https://www.over-yonder.net/~fullermd/projects/libcidr'
-  url 'http://bazaar.launchpad.net/~fullermd/libcidr/trunk/tarball/302'
-  sha256 '44d99c491a90d01baf0e074f6b5bac48e7eab93e97835819f631ef2a8bd12935'
+  url 'https://www.over-yonder.net/~fullermd/projects/libcidr/libcidr-1.2.3.tar.xz'
+  sha256 'afbe266a9839775a21091b0e44daaf890a46ea4c2d3f5126b3048d82b9bfbbc4'
 
-  # This patch can be viewed and edited via https://gist.github.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8
-  # This path can also be accessed via homebrew-shopify/patch/mac_os_libcidr_support.patch
   patch do
-    url "https://gist.githubusercontent.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8/raw/151aabb91fe183ca3d18521c29f90007aa690592/mac_os_libcidr_support.patch"
-    sha256 "76459e78199b2ea94f0522af24262ea259fb74e5ba38acd9619a58390eafd599"
+    url "https://gist.githubusercontent.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8/raw/cd2d5d2c3737f585fc71226786ea82279bf36645/mac_os_libcidr_support.patch"
+    sha256 "92b72bdc3fab7c95f3689d451d67848b1c4d1d9da58561e3db6e3d5ca57cb399"
   end
 
   def install
-    Dir.chdir("./libcidr/trunk")
     system "./mkgmake.sh"
     system "make"
     lib.install "./src/libcidr.dylib"
