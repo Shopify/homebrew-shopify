3e72d2602b76e28036b1c8dded7f61569ba184a4
Francisco Mejia <francisco.mejia@shopify.com>
2018-01-27 11:34:30 -0500

    Add mac_os_libcidr_support.patch to the repo

--- libcidr-shopify.rb
+++ libcidr-shopify.rb
@@ -4,6 +4,8 @@ class LibcidrShopify < Formula
   url 'http://bazaar.launchpad.net/~fullermd/libcidr/trunk/tarball/302'
   sha256 '44d99c491a90d01baf0e074f6b5bac48e7eab93e97835819f631ef2a8bd12935'
 
+  # This patch can be viewed and edited via https://gist.github.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8
+  # This path can also be accessed via homebrew-shopify/patch/mac_os_libcidr_support.patch
   patch do
     url "https://gist.githubusercontent.com/fmejia97/ad72a1f2b5d4b77a2921f32d861850b8/raw/151aabb91fe183ca3d18521c29f90007aa690592/mac_os_libcidr_support.patch"
     sha256 "76459e78199b2ea94f0522af24262ea259fb74e5ba38acd9619a58390eafd599"
