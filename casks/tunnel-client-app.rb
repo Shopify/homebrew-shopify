05cf9a6a7a62fa196bfea4e6a92149c21339e21c
Nick <nick.wu@shopify.com>
2018-03-05 12:38:11 -0500

    Change tunnel-client-app version to 0.1.0, update binary

--- casks/tunnel-client-app.rb
+++ casks/tunnel-client-app.rb
@@ -1,8 +1,8 @@
 cask 'tunnel-client-app' do
-  version '1.0.0'
-  sha256 'bc8dcb14d803e098facb1373e37123bcd74a24cc2667167c7953a09f3819e985'
+  version '0.1.0'
+  sha256 'ef1398a235b6e1d7ad2fd7e0a9d5dbf3d1b5a086bb171cc615943bae244fea29'
 
-  url 'https://storage.googleapis.com/tunnel-client-app-binaries/TunnelClient.app.tar.gz'
+  url 'https://storage.googleapis.com/tunnel-client-app-binaries/TunnelClient.app-c1382d0e6f4607b9739a0422468000536491f0e8.tar.gz'
   name 'Tunnel Client App'
   homepage 'https://github.com/Shopify/tunnel-client-app'
 
