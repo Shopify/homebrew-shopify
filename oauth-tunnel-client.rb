399bbcd64f9797c23aa53a7f16d3d300a652cba3
Nick <nick.wu@shopify.com>
2018-03-20 13:37:11 -0400

    Use oauth-tunnel-client built from master

--- oauth-tunnel-client.rb
+++ oauth-tunnel-client.rb
@@ -8,7 +8,7 @@ class OauthTunnelClient < Formula
   desc 'Create a secure local proxy with Shopify GCP services'
   homepage 'https://github.com/Shopify/oauth-tunnel-client'
   url 'https://storage.googleapis.com/oauth-tunnel-binaries/oauth-tunnel-client-binaries-b1dfd5b0f7d9c5abd0601192e1132fc03b1676f1.tar.gz?GoogleAccessId=pipa-production@shopify-docker-images.iam.gserviceaccount.com&Expires=1521647008&Signature=mPFZIYx069THKCQzm7VULjH6WRS0cbsn2x%2Fbt34zEKCseze3xlgiYgK1gr5LOjw4kXO3Cds7eEISoo5iQP8mqJfqZo3d2d3Od2%2Fw8coTn9ubb%2BXCEPYzBKhVxsz1XwVD1XZz4VjxA1gqWnz4fYHPxjseAIvTJkySmirday9EVcY2iUM5AY3v4MFxiJiYas7ngSwkHA5SeToo%2Fq1d%2B%2BO3IqkEmgLrT2J5TU%2FcPqnzPybiQg4rvhCZ7dmv3Fpdooyl4aAQxvo6zDjrwNSFjr2ARnoFiSzTmxiaPfpQtbnIfmSCVBcvRie%2Bn7NkvzbJDqxQtXw7NlcOAWyoPwfhMg73oA%3D%3D', using: GoogleStorageDownloadStrategy 
-  sha256 '40e8d8de95cbfd19fe61278cbcc0e6c937670d7bdb7924110d2d222080b53e3a'
+  sha256 '5792276bab2151408320f10d5b85480e52a3d95eaff260eb2577d8288bdade44'
   version "0.2.1"
 
   def install
