1c8bcdb5d715024e3ed6bd8203d407a8188c8c7a
Andrew Ryder <andrew.ryder@shopify.com>
2018-07-13 09:57:53 -0400

    update tunnel client paths for gcs

--- casks/tunnel-client-app.rb
+++ casks/tunnel-client-app.rb
@@ -3,7 +3,7 @@ cask 'tunnel-client-app' do
   sha256 'e3ea835ed22d4705ccaa1cc9718e5194fa7586047c5492130ec60a0fef853917'
 
   depends_on formula: 'oauth-tunnel-client'
-  url 'https://storage.googleapis.com/tunnel-client-app-binaries/TunnelClient.app-10ee5ff10a2d292ea63e3e7c2be1b40544cdfe1f.tar.gz'
+  url 'https://storage.googleapis.com/binaries.shopifykloud.com/tunnel-client-app/TunnelClient.app-10ee5ff10a2d292ea63e3e7c2be1b40544cdfe1f.tar.gz'
   name 'Tunnel Client App'
   homepage 'https://github.com/Shopify/tunnel-client-app'
 
