147a92cc26605fb717d392d8192814ea6c178f0a
Derek Y <derek.yu@shopify.com>
2018-10-13 17:29:42 -0400

    Add SHA256 check to docker-ssh-agent-forward.rb
    Based on a HackerOne report, it appears as though this does not properly authenticate the source repository. 
    We can currently point towards the master branch, but this will clearly break when if the repo updates.
    
    I will leave this here as an open PR for now.

--- docker-ssh-agent-forward.rb
+++ docker-ssh-agent-forward.rb
@@ -2,6 +2,8 @@ class DockerSshAgentForward < Formula
   desc "Forward SSH agent socket into a container"
   homepage "https://github.com/avsm/docker-ssh-agent-forward"
   head "https://github.com/avsm/docker-ssh-agent-forward.git"
+  url "https://github.com/avsm/docker-ssh-agent-forward/archive/master.tar.gz"
+  sha256 "3f6b11c72fe26a7228074d453bf13c54d03be09a11a7e8f1277c6ec9a411ecda"
 
   def install
     ENV['PATH']   = "#{ENV['PATH']}:/usr/local/bin"
