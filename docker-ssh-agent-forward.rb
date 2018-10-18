741ece8fd09b2beb7da11668458195f77d242883
Derek Y <derek.yu@shopify.com>
2018-10-18 10:44:17 -0400

    Update SHA256 and url

--- docker-ssh-agent-forward.rb
+++ docker-ssh-agent-forward.rb
@@ -1,9 +1,9 @@
 class DockerSshAgentForward < Formula
   desc "Forward SSH agent socket into a container"
-  homepage "https://github.com/avsm/docker-ssh-agent-forward"
-  head "https://github.com/avsm/docker-ssh-agent-forward.git"
-  url "https://github.com/avsm/docker-ssh-agent-forward/archive/master.tar.gz"
-  sha256 "3f6b11c72fe26a7228074d453bf13c54d03be09a11a7e8f1277c6ec9a411ecda"
+  homepage "https://github.com/Shopify/docker-ssh-agent-forward"
+  head "https://github.com/Shopify/docker-ssh-agent-forward.git"
+  url "https://github.com/Shopify/docker-ssh-agent-forward/archive/1.0.tar.gz"
+  sha256 "75d111b84cffbb6caed6a555cf88263a8079b609d2d0bb484afefdbb01f91251"
 
   def install
     ENV['PATH']   = "#{ENV['PATH']}:/usr/local/bin"
