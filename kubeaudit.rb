e5dbc1dda3083827acb4559cfa597256bb4a274f
Navraj Singh Chhina <navuchhina@live.com>
2019-03-27 15:31:50 -0400

    update to 0.5.2

--- kubeaudit.rb
+++ kubeaudit.rb
@@ -1,8 +1,8 @@
 class Kubeaudit < Formula
   desc "kubeaudit helps you audit your Kubernetes clusters against common security controls"
-  homepage "https://github.com/shopify/kubeaudit"
-  url "https://github.com/Shopify/kubeaudit/releases/download/v0.5.1/kubeaudit_0.5.1_darwin_amd64.tar.gz"
-  sha256 "eed75fb8cf094f2cf9e1cc422f4df9ebab51c43b68d62c60f6c712b9bbee1d21"
+  homepage "https://github.com/Shopify/kubeaudit"
+  url "https://github.com/Shopify/kubeaudit/releases/download/v0.5.2/kubeaudit_0.5.2_darwin_amd64.tar.gz"
+  sha256 "8748d00ef280691ca8ec70bb30b69c4deb172245ec9d39b1d6a2f31aef1bfb4a"
   def install
     bin.install "kubeaudit"
   end
