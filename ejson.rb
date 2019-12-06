e3a7fc67267008d49abb11e7038eb2c2640b8af2
Burke Libbey <burke@libbey.me>
2019-12-06 11:26:58 -0500

    Bump ejson to 1.2.1

--- ejson.rb
+++ ejson.rb
@@ -1,20 +1,12 @@
 class Ejson < Formula
   desc 'EJSON is a small library to manage encrypted secrets using asymmetric encryption.'
   homepage 'https://github.com/Shopify/ejson'
-  url 'https://github.com/Shopify/ejson/archive/v1.2.0.tar.gz'
+  url 'https://github.com/Shopify/ejson/archive/v1.2.1.tar.gz'
 
   bottle do
-    root_url "https://s3.amazonaws.com/burkelibbey"
-    cellar :any_skip_relocation
-    sha256 "b7add2e890b2dac8580cdd43de77fa7f979758f66d7ecb470ab1e9e73065c04c" => :high_sierra
-    sha256 "9f94ed3c05634be285c85e4488d3bdaf5a5e40306d99a3af5c968a38be011bbf" => :mojave
-  end
-
-  bottle do
-    cellar :any_skip_relocation
-    rebuild 1
     root_url "https://github.com/Shopify/homebrew-shopify/releases/download/bag-of-holding"
-    sha256 "f3d395b49bf9e877de1ef0a0e4b3f9ca7d78ef800fbd51bb723e0c2a52162288" => :catalina
+    cellar :any_skip_relocation
+    sha256 "cead7026c5bf46694ce67b5b664bd7a82570d76ff479de35dbb71d43a346b2d8" => :catalina
   end
 
   depends_on 'go' => :build
