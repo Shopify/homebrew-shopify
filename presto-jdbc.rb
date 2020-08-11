37ae275b2f9c4b42524917c734c8700a585e6b7b
noamhacker <noam.hacker@shopify.com>
2020-08-11 09:42:51 -0400

    fix homepage

--- presto-jdbc.rb
+++ presto-jdbc.rb
@@ -1,6 +1,6 @@
 class PrestoJdbc < Formula
   desc "JAR file for connecting to Presto over JDBC"
-  homepage "https://prestosql.io/docs/current/installation/jdbc.html"
+  homepage "https://prestosql.io/docs/338/installation/jdbc.html"
   url "https://repo1.maven.org/maven2/io/prestosql/presto-jdbc/338/presto-jdbc-338.jar"
   sha256 "3b96c19ec74909c7da4856eb7ff3f4bdff3965ce462826a3b74d5e405ff85754"
   version "338"
