634a3a13a15fdaf8d13777ea8f0d2eeca3eaed16
noamhacker <noam.hacker@shopify.com>
2020-08-11 09:32:12 -0400

    update presto jdbc to 338

--- presto-jdbc.rb
+++ presto-jdbc.rb
@@ -1,9 +1,9 @@
 class PrestoJdbc < Formula
   desc "JAR file for connecting to Presto over JDBC"
-  homepage "https://prestodb.github.io/docs/0.223/installation/jdbc.html"
-  url "https://repo1.maven.org/maven2/com/facebook/presto/presto-jdbc/0.223/presto-jdbc-0.223.jar"
-  sha256 "44ba89800257b085e8e0fc7dd95e58f9f767fa69f543f693b6f6f58937181aa5"
-  version "0.223"
+  homepage "https://prestosql.io/docs/current/installation/jdbc.html"
+  url "https://repo1.maven.org/maven2/io/prestosql/presto-jdbc/338/presto-jdbc-338.jar"
+  sha256 "3b96c19ec74909c7da4856eb7ff3f4bdff3965ce462826a3b74d5e405ff85754"
+  version "338"
 
   def install
     libexec.install "presto-jdbc-#{version}.jar"
