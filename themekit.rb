f99af8446409a2da0058ce1a16a0bd6d40d22c94
Andy Waite <andyw8@users.noreply.github.com>
2020-08-31 14:16:18 -0400

    Update Theme Kit to v1.1.0

--- themekit.rb
+++ themekit.rb
@@ -1,8 +1,8 @@
 class Themekit < Formula
   desc 'Theme Kit is a tool kit for manipulating shopify themes'
   homepage 'https://shopify.github.io/themekit/'
-  url 'https://shopify-themekit.s3.amazonaws.com/v1.0.3/darwin-amd64/theme'
-  sha256 '17eda69c0f729abaf34be23ae7bb8095627d918d03c04373e321dd43a54e5a9e'
+  url 'https://shopify-themekit.s3.amazonaws.com/v1.1.0/darwin-amd64/theme'
+  sha256 '097056b2ec18281fea3f52993d4c6d0ed63c9ccaa028cd72b354c6265e7f43c4'
   version 'v1.0.3'
 
   def install
