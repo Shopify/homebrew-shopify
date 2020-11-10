a2a2d3355ce2456b31fdd29d24abd4129f2155ab
cjones-shopify <73258133+cjones-shopify@users.noreply.github.com>
2020-11-10 11:07:05 -0500

    bumps Voucher client to v2.5.2 (#198)
    Co-authored-by: Catherine Jones <31020910+catherinejones@users.noreply.github.com>

--- voucher_client.rb
+++ voucher_client.rb
@@ -1,9 +1,9 @@
 class VoucherClient < Formula
   desc "Voucher Client is a tool for connecting to a running Voucher server."
   homepage "https://github.com/Shopify/voucher"
-  url "https://github.com/Shopify/voucher/releases/download/v1.0.0/voucher_1.0.0_Darwin_x86_64.tar.gz"
-  version "v1.0.0"
-  sha256 "8389111183a09701b9c3442e22481432d3eac31f287402beaebaab1f20defdea"
+  url "https://github.com/Shopify/voucher/releases/download/v2.5.2/voucher_client_2.5.2_Darwin_x86_64.tar.gz"
+  version "v2.5.2"
+  sha256 "e36a04e4b52d7be9888e0810a5fccbded5e5a0bbdedd03fb52a6dbb549e3f78d"
 
   def install
     bin.install "voucher_client"
