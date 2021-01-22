23cc74873b714053fbc0a86998999094f0e67f89
Tim Anema <tim.anema@shopify.com>
2021-01-22 09:53:44 -0500

    Correct the sha on themekit formula

--- themekit.rb
+++ themekit.rb
@@ -3,7 +3,7 @@ class Themekit < Formula
   desc 'Theme Kit is a tool kit for manipulating shopify themes'
   homepage 'https://shopify.github.io/themekit/'
   url "https://shopify-themekit.s3.amazonaws.com/#{version}/darwin-amd64/theme"
-  sha256 'fd72d6d06acdc1122e0399a25e4714e3b887053fab0b76a3fa5c97260d99af35'
+  sha256 '483a660c655cfad28787106bcaea3c52d09ee1dcd8007e05007d223ae8aaf4ea'
 
   def install
     bin.install 'theme'
