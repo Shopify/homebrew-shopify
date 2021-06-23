efdfc3d5ec5648f587d72375b273eace208d46d8
Karen Xie <karen.xie@shopify.com>
2021-06-23 14:34:31 -0400

    Bump themekit version (#262)

--- themekit.rb
+++ themekit.rb
@@ -1,9 +1,9 @@
 class Themekit < Formula
-  version 'v1.2.0'
+  version 'v1.3.0'
   desc 'Theme Kit is a tool kit for manipulating shopify themes'
   homepage 'https://shopify.github.io/themekit/'
   url "https://shopify-themekit.s3.amazonaws.com/#{version}/darwin-amd64/theme"
-  sha256 '5563591b36b9eb0e63ae7496d9ab9a08056d123ad81d2abf03e1bdac9ffd64ad'
+  sha256 '5b091f20bddf97bba167bb029ce74d5236d824fa4e210e549b7c2d06941018e5'
 
   def install
     bin.install 'theme'
