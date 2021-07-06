33e2e7e6b0366504a34735fc7d4c6dc8c67fa9ef
Austen Lacy <austenlacy@gmail.com>
2021-07-06 18:47:48 -0400

    fix shas for trino casks

--- trino-jdbc.rb
+++ trino-jdbc.rb
@@ -2,7 +2,7 @@ class TrinoJdbc < Formula
   desc "JAR file for connecting to Trino over JDBC"
   homepage "https://trino.io/docs/current/installation/jdbc.html"
   url "https://repo1.maven.org/maven2/io/trino/trino-jdbc/359/trino-jdbc-359.jar"
-  sha256 "8f41e467762b5bf039b59717c05bac011d5dfd0e"
+  sha256 "4ec6a83ccf9cdcab60aed0eca644f9a8a8cec256d0a2fc9e00a298be6d054098"
   version "359"
 
   def install
