4a12eb9a62905ba24447c9853845876878af279a
Austen Lacy <austenlacy@gmail.com>
2021-07-06 16:52:39 -0400

    fix typo

--- trino-cli.rb
+++ trino-cli.rb
@@ -1,5 +1,5 @@
 class TrinoCli < Formula
-  desc "Trino CLI executable to connect and run queries against Presto"
+  desc "Trino CLI executable to connect and run queries against Trino"
   homepage "https://trino.io/docs/current/installation/cli.html"
   url "https://repo1.maven.org/maven2/io/trino/trino-cli/359/trino-cli-359-executable.jar"
   sha256 "ff7ad5004b47eb5a1d2f40d122efa5b053c9cebb"
