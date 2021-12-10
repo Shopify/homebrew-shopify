c683e811cdd7373b6e3fe05f8277a2d219e9430f
Lisa Ugray <lisa.ugray@shopify.com>
2021-12-10 12:19:41 -0500

    Make luajit-shopify work on arm
    Cribbed formula changes from upstream homebrew package but keeping our
    beta pin.

--- luajit-shopify.rb
+++ luajit-shopify.rb
@@ -5,15 +5,10 @@ class LuajitShopify < Formula
   sha256 "1ad2e34b111c802f9d0cdf019e986909123237a28c746b21295b63c9e785d9c3"
   head "http://luajit.org/git/luajit-2.0.git"
 
-  # bottle do
-  #   root_url "http://burkelibbey.s3.amazonaws.com"
-  #   rebuild 1
-  #   sha256 "614270751ee3379f1aa931accefc6d29d0b97b3bdf19e1ef550bb8b32e0d7954" => :sierra
-  # end
-
   bottle do
     root_url "https://github.com/Shopify/homebrew-shopify/releases/download/bag-of-holding"
-    sha256 cellar: :any, catalina: "0d8d72fb092e26c30bc0519626044b8d49bdc1e1a2301eab58ccbe0796233ec2"
+    sha256 cellar: :any, arm64_monterey: "90dafd36b7efaf2ec8e5a9273b28584396220b55fd69e50911b8afa21a1e3a7a"
+    sha256 cellar: :any, monterey: "aec5523c766b1c75e50f9b105b9e165a5cb991f8020a42106f5a11c4cc161a7e"
   end
 
   conflicts_with "luajit", :because => "shopify uses luajit 2.1. `brew uninstall luajit` first"
@@ -35,31 +30,39 @@ class LuajitShopify < Formula
     # Per https://luajit.org/install.html: If MACOSX_DEPLOYMENT_TARGET
     # is not set then it's forced to 10.4, which breaks compile on Mojave.
     ENV["MACOSX_DEPLOYMENT_TARGET"] = MacOS.version
-      
-    ENV.O2 # Respect the developer's choice.
 
     args = %W[PREFIX=#{prefix}]
-    cflags = []
-    cflags << "-DLUAJIT_ENABLE_LUA52COMPAT" if build.with? "52compat"
 
-    args << "XCFLAGS=#{cflags.join(" ")}" if cflags.present?
-
-    # This doesn't yet work under superenv because it removes "-g"
-    args << "CCDEBUG=-g" if build.with? "debug"
-
-    # The development branch of LuaJIT normally does not install "luajit".
-    args << "INSTALL_TNAME=luajit"
+    # Build with 64-bit support
+    args << "XCFLAGS=-DLUAJIT_ENABLE_GC64" if build.head?
 
     system "make", "amalg", *args
     system "make", "install", *args
+
+    # Unstable branch doesn't install symlink for luajit.
+    # This breaks tools like `luarock` who requires the `luajit` bin to be present.
+    bin.install_symlink Dir[bin/"luajit-*"].first => "luajit" if build.head?
 
     # LuaJIT doesn't automatically symlink unversioned libraries:
     # https://github.com/Homebrew/homebrew/issues/45854.
     lib.install_symlink lib/"libluajit-5.1.dylib" => "libluajit.dylib"
     lib.install_symlink lib/"libluajit-5.1.a" => "libluajit.a"
 
+    # Fix path in pkg-config so modules are installed
+    # to permanent location rather than inside the Cellar.
+    inreplace lib/"pkgconfig/luajit.pc" do |s|
+      s.gsub! "INSTALL_LMOD=${prefix}/share/lua/${abiver}",
+              "INSTALL_LMOD=#{HOMEBREW_PREFIX}/share/lua/${abiver}"
+      s.gsub! "INSTALL_CMOD=${prefix}/${multilib}/lua/${abiver}",
+              "INSTALL_CMOD=#{HOMEBREW_PREFIX}/${multilib}/lua/${abiver}"
+      unless build.head?
+        s.gsub! "Libs:",
+                "Libs: -pagezero_size 10000 -image_base 100000000"
+      end
+    end
+
     # Having an empty Lua dir in lib/share can mess with other Homebrew Luas.
-    %W[ #{lib}/lua #{share}/lua ].each { |d| rm_rf d }
+    %W[#{lib}/lua #{share}/lua].each { |d| rm_rf d }
   end
 
   test do
