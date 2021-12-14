e4ac7ab57e7c09d9c3d41eff54a3e92f17c5e3f2
Lisa Ugray <lisa.ugray@shopify.com>
2021-12-14 13:45:16 -0500

    ejson 1.3.1

--- ejson.rb
+++ ejson.rb
@@ -1,13 +1,13 @@
 class Ejson < Formula
   desc 'EJSON is a small library to manage encrypted secrets using asymmetric encryption.'
   homepage 'https://github.com/Shopify/ejson'
-  url 'https://github.com/Shopify/ejson/archive/v1.3.0.tar.gz'
-  sha256 "4f09d99fc8546676fef945aeb9878bafae0f54fcfc4c188fe8bbd87e4146a60f"
+  url 'https://github.com/Shopify/ejson/archive/v1.3.1.tar.gz'
+  sha256 '5c2bd9f1bec018f64be97d68a0ef1dc48763613e0c85d731ba96705b0720b779'
 
   bottle do
-    root_url "https://github.com/Shopify/homebrew-shopify/releases/download/bag-of-holding"
-    sha256 cellar: :any_skip_relocation, arm64_monterey: "d8b39327e7d88a5523ae64449b80c51a67f1e8419770e44831a30d1ad879c689"
-    sha256 cellar: :any_skip_relocation, monterey: "fe5b00098bb320eee19250f1e6317242cd15cbe8bc538c899acebafcdeee62db"
+    root_url 'https://github.com/Shopify/homebrew-shopify/releases/download/bag-of-holding'
+    sha256 cellar: :any_skip_relocation, arm64_monterey: '6f308923a7d1e00640692bcf19b30d491cb6fcb5836a091931d65a57fa0e8b12'
+    sha256 cellar: :any_skip_relocation, monterey: '4ffdee0338de07d3983b2b2bba0e0c20cad9cdcb9fd52369228a144956374a14'
   end
 
   depends_on 'go' => :build
