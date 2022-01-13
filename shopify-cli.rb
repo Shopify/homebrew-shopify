d579036ea9d97fb70edee42e00d9ead6722bee51
Ariel Caplan <ariel.caplan@shopify.com>
2022-01-13 10:43:12 +0200

    Depend on Ruby 3

--- shopify-cli.rb
+++ shopify-cli.rb
@@ -48,7 +48,7 @@ class ShopifyCli < Formula
   url "shopify-cli", using: RubyGemsDownloadStrategy
   version "2.8.0"
   sha256 "92c60b57ca784d8bed6378de6a31444523b91026e594e9db56c85b40ebfb3b78"
-  depends_on "ruby"
+  depends_on "ruby" => "3"
   depends_on "git"
 
   def install
