f5f2ae0d1baa736e7a6bf62621e758675527be7c
Atamjeet Singh <atamjeet.singh@shopify.com>
2022-01-27 13:31:26 -0500

    Support oauth tunnel on linux (#340)
    * Support oath tunnel client on Linux
    
    * Support Linux in oauth-tunnel-client
    
    * Add support for Linux
    
    * drop whitespace
    
    * Add comments on manual start

--- oauth-tunnel-client.rb
+++ oauth-tunnel-client.rb
@@ -4,14 +4,27 @@ class OauthTunnelClient < Formula
   url 'https://storage.googleapis.com/binaries.shopifykloud.com/oauth-tunnel/oauth-tunnel-client-c3219ecfeef1965c6524040f99aea53bdbfde9af.tar.gz'
   sha256 'c1b3d5b5ee6b92f4f44795b23e76742398759f21b235a386f7cf9d507cbc099a'
   version "1.0.0"
+  plist_options manual: "export GIN_MODE=release && #{HOMEBREW_PREFIX}/opt/oauth-tunnel-client/bin/oauth-tunnel-client"
 
-  arch = Hardware::CPU.intel? ? "amd64" : "arm64"
-  @@binary_name = "oauth-tunnel-client_darwin_#{arch}" 
+
+  case
+  when OS.mac? && Hardware::CPU.intel?
+    @@binary_name = "oauth-tunnel-client_darwin_amd64" 
+  when OS.mac? && Hardware::CPU.arm?
+    @@binary_name = "oauth-tunnel-client_darwin_arm64"
+  when OS.linux? && Hardware::CPU.intel?
+    @@binary_name = "oauth-tunnel-client_linux_386"
+  when OS.linux? && Hardware::CPU.arm?
+    @@binary_name = "oauth-tunnel-client_linux_arm64" 
+  else
+    odie "Unexpected platform!"
+  end
 
   def install
     bin.install({@@binary_name => 'oauth-tunnel-client'})
     mkdir_p var/"log/oauth-tunnel-client"
   end
+
   def plist
     home = Dir.home
     <<~EOS
