1dc5e5510349f246840610626e25cc8b9b40e369
Daniel Lobl <daniel.lobl@shopify.com>
2022-06-22 11:50:30 +0200

    remove unneeded dependencies

--- shopify-vips.rb
+++ shopify-vips.rb
@@ -10,11 +10,8 @@ class ShopifyVips < Formula
   depends_on "pkg-config" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "cairo"
-  depends_on "cfitsio"
   depends_on "cgif"
   depends_on "fftw"
-  depends_on "fontconfig"
   depends_on "freetype"
   depends_on "gdk-pixbuf"
   depends_on "gettext"
@@ -25,48 +22,25 @@ class ShopifyVips < Formula
   depends_on "imath"
   depends_on "jpeg-xl"
   depends_on "libexif"
-  depends_on "libgsf"
   depends_on "libheif"
   depends_on "libimagequant"
-  depends_on "libmatio"
   depends_on "libpng"
   depends_on "librsvg"
   depends_on "libtiff"
   depends_on "libxml2"
-  depends_on "little-cms2"
   depends_on "mozjpeg"
   depends_on "openexr"
-  depends_on "openjpeg"
-  depends_on "openslide"
   depends_on "pango"
-  depends_on "poppler"
   depends_on "webp"
-
   uses_from_macos "expat"
   uses_from_macos "zlib"
 
-
   def install
-    args = %W[
-      --prefix=#{prefix}
-      --backend=ninja
-    ]
-
-    system "meson setup build", *args
-    system "meson", "install", "-C build"
+    system "meson", "setup", "build", "--prefix=#{prefix}", "-Dmagick-package=GraphicsMagick", "--buildtype=release"
+    system "meson", "install", "-C", "build"
   end
 
   test do
     system "#{bin}/vips", "-l"
-    cmd = "#{bin}/vipsheader -f width #{test_fixtures("test.png")}"
-    assert_equal "8", shell_output(cmd).chomp
-
-    # --trellis-quant requires mozjpeg, vips warns if it's not present
-    cmd = "#{bin}/vips jpegsave #{test_fixtures("test.png")} #{testpath}/test.jpg --trellis-quant 2>&1"
-    assert_equal "", shell_output(cmd)
-
-    # [palette] requires libimagequant, vips warns if it's not present
-    cmd = "#{bin}/vips copy #{test_fixtures("test.png")} #{testpath}/test.png[palette] 2>&1"
-    assert_equal "", shell_output(cmd)
   end
 end
