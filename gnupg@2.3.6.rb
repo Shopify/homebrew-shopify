71d6816109e3d6942fe29dcd7687b640f63e9c51
Misty De Méo <misty.demeo@shopify.com>
2022-07-12 11:00:07 -0700

    Backport gnupg 2.3.6
    2.3.7 has a bug that breaks Yubikey smart card support.

---
+++ gnupg@2.3.6.rb
@@ -0,0 +1,84 @@
+class GnupgAT236 < Formula
+  desc "GNU Pretty Good Privacy (PGP) package"
+  homepage "https://gnupg.org/"
+  url "https://gnupg.org/ftp/gcrypt/gnupg/gnupg-2.3.6.tar.bz2"
+  sha256 "21f7fe2fc5c2f214184ab050977ec7a8e304e58bfae2ab098fec69f8fabda9c1"
+  license "GPL-3.0-or-later"
+
+  livecheck do
+    url "https://gnupg.org/ftp/gcrypt/gnupg/"
+    regex(/href=.*?gnupg[._-]v?(\d+(?:\.\d+)+)\.t/i)
+  end
+
+  depends_on "pkg-config" => :build
+  depends_on "gettext"
+  depends_on "gnutls"
+  depends_on "libassuan"
+  depends_on "libgcrypt"
+  depends_on "libgpg-error"
+  depends_on "libksba"
+  depends_on "libusb"
+  depends_on "npth"
+  depends_on "pinentry"
+
+  uses_from_macos "sqlite", since: :catalina
+
+  on_linux do
+    depends_on "libidn"
+  end
+
+  conflicts_with "homebrew/core/gnupg"
+
+  def install
+    libusb = Formula["libusb"]
+    ENV.append "CPPFLAGS", "-I#{libusb.opt_include}/libusb-#{libusb.version.major_minor}"
+
+    system "./configure", "--disable-dependency-tracking",
+                          "--disable-silent-rules",
+                          "--prefix=#{prefix}",
+                          "--sbindir=#{bin}",
+                          "--sysconfdir=#{etc}",
+                          "--enable-all-tests",
+                          "--with-pinentry-pgm=#{Formula["pinentry"].opt_bin}/pinentry"
+    system "make"
+    system "make", "check"
+    system "make", "install"
+
+    # Configure scdaemon as recommended by upstream developers
+    # https://dev.gnupg.org/T5415#145864
+    if OS.mac?
+      # write to buildpath then install to ensure existing files are not clobbered
+      (buildpath/"scdaemon.conf").write <<~EOS
+        disable-ccid
+      EOS
+      pkgetc.install "scdaemon.conf"
+    end
+  end
+
+  def post_install
+    (var/"run").mkpath
+    quiet_system "killall", "gpg-agent"
+  end
+
+  test do
+    (testpath/"batch.gpg").write <<~EOS
+      Key-Type: RSA
+      Key-Length: 2048
+      Subkey-Type: RSA
+      Subkey-Length: 2048
+      Name-Real: Testing
+      Name-Email: testing@foo.bar
+      Expire-Date: 1d
+      %no-protection
+      %commit
+    EOS
+    begin
+      system bin/"gpg", "--batch", "--gen-key", "batch.gpg"
+      (testpath/"test.txt").write "Hello World!"
+      system bin/"gpg", "--detach-sign", "test.txt"
+      system bin/"gpg", "--verify", "test.txt.sig"
+    ensure
+      system bin/"gpgconf", "--kill", "gpg-agent"
+    end
+  end
+end
