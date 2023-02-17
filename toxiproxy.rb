136013886ccddc1905e609e0e1512f241244de59
Michael Nikitochkin <michael.nikitochkin@shopify.com>
2023-02-17 14:31:21 +0100

    Replace plist with service directive

--- toxiproxy.rb
+++ toxiproxy.rb
@@ -59,25 +59,8 @@ class Toxiproxy < Formula
     end
   end
 
-  plist_options manual: "toxiproxy"
-
-  def plist; <<~EOS
-    <?xml version="1.0" encoding="UTF-8"?>
-    <!DOCTYPE plist PUBLIC "-//Apple//DTD PLIST 1.0//EN" "http://www.apple.com/DTDs/PropertyList-1.0.dtd">
-    <plist version="1.0">
-      <dict>
-        <key>Label</key>
-        <string>#{plist_name}</string>
-        <key>ProgramArguments</key>
-        <array>
-          <string>#{opt_bin}/toxiproxy-server</string>
-        </array>
-        <key>RunAtLoad</key>
-        <true/>
-        <key>KeepAlive</key>
-        <true/>
-      </dict>
-    </plist>
-    EOS
+  service do
+    run [bin/"toxiproxy-server"]
+    keep_alive true
   end
 end
