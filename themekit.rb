1979aca33ebdd03baedb2244b238ba30c32e765a
Gonzalo Riestra <gonzalo.riestra@shopify.com>
2024-03-19 11:07:46 +0100

    Update themekit.rb

--- themekit.rb
+++ themekit.rb
@@ -1,9 +1,9 @@
 class Themekit < Formula
-  version 'v1.3.1'
+  version 'v1.3.2'
   desc 'Theme Kit is a tool kit for manipulating shopify themes'
   homepage 'https://shopify.github.io/themekit/'
   url "https://shopify-themekit.s3.amazonaws.com/#{version}/darwin-amd64/theme"
-  sha256 'c7ff4626d0dd680d13c3113f4c84a069bf7640b5ac7ed05ab6e1c48ddfa064f5'
+  sha256 'c81af2eb4e8c1f55c05c11a5d6716e2e651712f198e99b8c85da277a06a2dc8e'
 
   def install
     bin.install 'theme'
